6a9bd756610ab0929a67ad10fd2304a31ab5f039
J S Suhas <j.s.suhas@live.in>
2016-09-11 15:13:58 +0530

    Merged CPP env and agent. Need @SaiPraveenB to complete a bit of the CPP env part.

--- planning/cpp_mc.h
+++ planning/cpp_mc.h
@@ -5,88 +5,193 @@
 #ifndef SUPER_DUPER_OCTO_LAMP_CPP_MC_H
 #define SUPER_DUPER_OCTO_LAMP_CPP_MC_H
 
-class MCPlanner {
-    MCPlanner(MinecraftPsuedoEnvironment env, const int num_trajs, const float learning_rate, const float gamma,
-              const int max_num_steps) : env(env), num_trajectories(num_trajs), alpha(learning_rate), gamma(gamma),
-                                         max_num_steps(max_num_steps) {
-        const auto &env_shape = env.GetShape();
-        const int num_actions = env.GetNumActions();
-        q_values = std::vector<std::vector<std::vector<float>>>(env_shape[0],std::vector<std::vector<float>>(env_shape[1],std::vector<float>(num_actions,0.0f)));
+// Function to generate primitive kernels
+std::vector<std::vector<bool>> GetPrimitiveKernel(const int dir, const std::pair<int,int> kern_shape) {
+    std::vector<std::vector<bool>> kernel(kern_shape.first, std::vector<bool>(false,kern_shape.second));
+    switch(dir) {
+        case 0 :
+            for(int i=0;i<kern_shape.first;i++)
+                for(int j=0;j<=i;j++)
+                    kernel[i][j] = true;
+            break;
+        case 1 :
+            for(int i=0;i<kern_shape.first;i++)
+                for(int j=0;j<=kern_shape.first-1 - i;j++)
+                    kernel[i][j] = true;
+            break;
+        case 2 :
+            for(int i=0;i<kern_shape.first;i++)
+                for(int j=i;j<=kern_shape.first - 1;j++)
+                    kernel[i][j] = true;
+            break;
+        case 3 :
+            for(int i=0;i<kern_shape.first;i++)
+                for(int j=kern_shape.first-1 - i;j<=kern_shape.first - 1;j++)
+                    kernel[i][j] = true;
+            break;
     }
 
-    const std::vector<float> plan()
-    {
-        std::random_device                  rand_dev;
-        std::mt19937                        generator(rand_dev());
-        std::uniform_int_distribution<int>  distr(0, q_values[0][0].size() - 1);
+    return kernel;
+}
 
-        // Obtain trajectories
-        std::vector<std::vector<std::tuple<std::pair<int,int>, int, float, std::pair<int,int>>>> trajectories;
+std::vector<std::vector<bool>> BitwiseAND(const std::vector<std::vector<bool>>& first,
+                                          const std::vector<std::vector<bool>>& second) {
+    std::vector<std::vector<bool>> ret = std::vector<std::vector<bool>>(first.size(),std::vector<bool>(first[0].size(),false));
+    for(int i=0;i<first.size();i++)
+        for(int j=0;j<first[0].size();j++)
+            ret[i][j] = first[i][j] && second[i][j];
+
+    return ret;
+}
+std::vector<std::vector<bool>> GetKernel(const int direction, const std::pair<int,int> kern_shape) {
+    std::vector<std::vector<bool>> kernel;
+    static bool first_run = true;
+    static std::vector<std::vector<std::vector<bool>>> primitive_kernels = {GetPrimitiveKernel(0,kern_shape),
+                                                                            GetPrimitiveKernel(1,kern_shape),
+                                                                            GetPrimitiveKernel(2,kern_shape),
+                                                                            GetPrimitiveKernel(3,kern_shape)};
+    static std::vector<std::vector<std::vector<bool>>> final_kernels;
+    if(first_run) {
+        final_kernels = {BitwiseAND(primitive_kernels[1], primitive_kernels[2]),
+                         BitwiseAND(primitive_kernels[0], primitive_kernels[3]),
+                         BitwiseAND(primitive_kernels[2], primitive_kernels[3]),
+                         BitwiseAND(primitive_kernels[0], primitive_kernels[1])};
+        first_run = false;
+    }
+
+    return final_kernels[direction];
+}
+
+// Python function call
+std::vector<int> GetActionValueEstimate(const std::vector<std::vector<float>>& rewards,
+                                        const std::pair<int,int> start_state, const std::pair<int,int> end_state,
+                                        const std::pair<int,int> kern_shape) {
+    // Define global params here.
+    std::pair<int,int> env_shape = std::make_pair(rewards.size(),rewards[0].size());
+    const float gamma = 0.9;
+    const auto& vis_kernels = {GetKernel(NORTH,kern_shape),GetKernel(SOUTH,kern_shape),
+                               GetKernel(EAST,kern_shape),GetKernel(WEST,kern_shape)};
+
+    /***************************** ENVIRONMENT ************************/
+    // Define environment here.
+    struct Environment {
+        const enum Directions {NORTH, SOUTH, EAST, WEST};
+
+        // Define env vars here.
+        const auto actions = { make_tuple(0,1,0), make_tuple(1,0,0),make_tuple(0,-1,0),make_tuple(-1,0,0),
+                               make_tuple(0,0,NORTH),make_tuple(0,0,SOUTH),make_tuple(0,0,EAST), make_tuple(0,0,WEST)};
+
+        // Weight for pseudo rewards.
+        const float pseudo_reward_multiplier = 1.0;
+        std::vector<std::vector<bool>> seen;
+        std::vector<std::vector<std::vector<bool>>> vis_kernels;
+        std::pair<int,int> kern_shape;
+        std::pair<int,int> env_shape;
+        std::pair<int,int> state;
+        const std::pair<int,int> end_state;
+
+        // Define env functions.
+        Environment(std::pair<int,int> env_shape, std::pair<int,int> kern_shape,
+                    const std::pair<int,int> start_state,const std::pair<int,int> end_state) {
+            seen = std::vector<std::vector<bool>>(env_shape.first,std::vector<bool>(env_shape.second,false));
+            vis_kernels = {GetKernel(NORTH,kern_shape),GetKernel(SOUTH,kern_shape),
+                           GetKernel(EAST,kern_shape),GetKernel(WEST,kern_shape)};
+            kern_shape = kern_shape;
+            env_shape = env_shape;
+            state = start_state;
+            end_state = end_state;
+        }
+
+        // Define env play() here.
+        std::tuple<std::pair<int,int>,float,bool> play(int action) {
+            // TODO(SaiPraveenB) : Can you fill in the delta visibility calculations and visibility updating?
+            int delta_vis;
+            // Calculate delta(visibility)
+            // Update visibility
+
+            // Calculate psuedo rewards, actual reward
+            const auto action_taken = actions[action];
+            int y_coord = (state.first+action_taken.first < 0 ? 0 : state.first+action_taken.first >= env_shape.first ? env_shape.first-1 : state.first+action_taken.first);
+            int x_coord = (state.second+action_taken.second < 0 ? 0 : state.second+action_taken.second >= env_shape.second? env_shape.second-1 : state.first+action_taken.second);
+            const auto new_state = std::make_pair(y_coord, x_coord);
+            float pseudo_reward = delta_vis*pseudo_reward_multiplier;
+            float total_reward = (action < 4 ? (new_state == state ? 0 : rewards[y_coord][x_coord]) : 0) + pseudo_reward;
+
+            state = new_state;
+
+            // Return (new state, reward, is_completed)
+            return std::make_tuple(new_state, total_reward, (state == end_state));
+        };
+    };
+
+    /***************************** AGENT ************************/
+    // Define agent params here.
+    const int num_trajectories = 200;
+    const float alpha = 0.05;
+    const int max_num_steps = 400;
+    const int num_actions = 8;
+
+    std::vector<std::vector<std::vector<float>>> q_values = q_values = std::vector<std::vector<std::vector<float>>>(env_shape[0],std::vector<std::vector<float>>(env_shape[1],std::vector<float>(num_actions,0.0f)));;
+
+    // Define agent behaviour here.
+    std::random_device                  rand_dev;
+    std::mt19937                        generator(rand_dev());
+    std::uniform_int_distribution<int>  distr(0, q_values[0][0].size() - 1);
+
+    // Obtain trajectories
+    std::vector<std::vector<std::tuple<std::pair<int,int>, int, float, std::pair<int,int>>>> trajectories;
 #pragma omp declare reduction (merge : std::vector<int> : omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end()))
 #pragma omp parallel for reduction(merge: trajectories)
-        for(int i=0;i<num_trajectories;i++) {
-            // Start state (2-tuple) [int]
-            std::vector < std::tuple < std::pair < int, int >, int, float, std::pair < int, int >> > cur_trajectory;
-            MinecraftPseudoEnvironment cur_env = env;
-            std::pair<int, int> cur_state = env.GetStartState();
-            bool is_completed = false;
-            int num_steps = 0;
-            while (!is_completed && num_steps < this->max_num_steps) {
-                // Choose a random action.
-                int cur_action = distr(generator);
+    for(int i=0;i<num_trajectories;i++) {
+        // Start state (2-tuple) [int]
+        std::vector < std::tuple < std::pair < int, int >, int, float, std::pair < int, int >> > cur_trajectory;
+        MinecraftPseudoEnvironment cur_env = env;
+        std::pair<int, int> cur_state(start_state);
+        bool is_completed = false;
+        int num_steps = 0;
+        while (!is_completed && num_steps < this->max_num_steps) {
+            // Choose a random action.
+            int cur_action = distr(generator);
 
-                // Play the action.
-                const auto &env_response = env.Play(cur_action);
+            // Play the action.
+            const auto &env_response = env.Play(cur_action);
 
-                std::pair<int, int> new_state = std::get<0>(env_response);
-                float reward = std::get<1>(env_response);
-                is_completed = std::get<2>(env_response);
+            std::pair<int, int> new_state = std::get<0>(env_response);
+            float reward = std::get<1>(env_response);
+            is_completed = std::get<2>(env_response);
 
-                // Add (s,a,r,s) to experience
-                cur_trajectory.push_back(std::make_tuple(cur_state, cur_action, reward, new_state));
+            // Add (s,a,r,s) to experience
+            cur_trajectory.push_back(std::make_tuple(cur_state, cur_action, reward, new_state));
 
-                // Update params
-                num_steps++;
-                cur_state = new_state;
-            }
-
-            trajectories.push_back(cur_trajectory);
+            // Update params
+            num_steps++;
+            cur_state = new_state;
         }
-        // Construct Q-values
-#pragma omp parallel for
-        for(const auto& traj : trajectories) {
-            for(const auto iter = traj.rbegin(); iter != traj.rend(); iter++) {
-                // Obtain (s,a,r,s) values
-                std::pair<int,int> new_state,old_state;
-                float reward;
-                int action;
-                std::tie(old_state, action, reward, new_state) = *iter;
-
-                // Obtain target
-                float target = reward + gamma * (*std::max_element(q_values[old_state.first][old_state.second].begin(), q_values[old_state.first][old_state.second].end()));
-
-                // Obtain TD error
-                float TD_error = (target - q_values[old_state.first][old_state.second][action]);
-
-                // Perform Q update
-#pragma omp atomic
-                q_values[old_state.first][old_state.second] += alpha * TD_error;
-            }
-        }
-
-        return q_values[start_state.first][start_state.second];
+        trajectories.push_back(cur_trajectory);
     }
+    // Compute Q-values
+#pragma omp parallel for
+    for(const auto& traj : trajectories) {
+        for(const auto iter = traj.rbegin(); iter != traj.rend(); iter++) {
+            // Obtain (s,a,r,s) values
+            std::pair<int,int> new_state,old_state;
+            float reward;
+            int action;
+            std::tie(old_state, action, reward, new_state) = *iter;
 
+            // Obtain target
+            float target = reward + gamma * (*std::max_element(q_values[old_state.first][old_state.second].begin(), q_values[old_state.first][old_state.second].end()));
 
-private:
-    MinecraftPsuedoEnvironment env;
-    const int num_trajectories;
-    const float alpha;
-    const float gamma;
-    const int max_num_steps;
+            // Obtain TD error
+            float TD_error = (target - q_values[old_state.first][old_state.second][action]);
 
-    std::vector <std::vector<std::vector < int>>>
-    q_values;
-};
+            // Perform Q update
+#pragma omp atomic
+            q_values[old_state.first][old_state.second] += alpha * TD_error;
+        }
+    }
+    return q_values[start_state.first][start_state.second];
+}
+
 
 #endif //SUPER_DUPER_OCTO_LAMP_CPP_MC_H
