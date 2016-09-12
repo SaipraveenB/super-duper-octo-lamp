cb1a5f0e251547ad9191882ecf44cd1124430753
J S Suhas <j.s.suhas@live.in>
2016-09-12 16:59:25 +0530

    Completed env part. Need to test.

--- planning/cpp_mc.h
+++ planning/cpp_mc.h
@@ -62,8 +62,28 @@ std::vector<std::vector<bool>> GetKernel(const int direction, const std::pair<in
     return final_kernels[direction];
 }
 
+// Make sure kernel.size() == kernel[0].size() == EVEN
+int GetDeltaAndUpdate(const std::vector<std::vector<bool>>& kernel, std::vector<std::vector<bool>> &visib, const std::pair<int,int> state) {
+    // Figure out i_min, i_max, j_min, j_max for kernel.
+    int length = kernel.size();
+    int num_pts = length/2;
+    int y_min = std::max(state.first - num_pts, 0);
+    int y_max = std::min(state.first + num_pts, visib.size() - 1);
+    int x_min = std::max(state.second - num_pts, 0);
+    int x_max = std::min(state.second + num_pts, visib[0].size() - 1);
+
+    int num_changed = 0;
+    for(int y = y_min; y<= y_max; y++)
+        for(int x = x_min; x<= x_max; x++) {
+            num_changed += static_cast<int>(~visib[state.first + y][state.second + x] && kernel[y][x]);
+            visib[state.first + y][state.second + x] |= kernel[y][x];
+        }
+
+    return num_changed;
+}
+
 // Python function call
-std::vector<int> GetActionValueEstimate(const std::vector<std::vector<float>>& rewards,
+std::vector<float> GetActionValueEstimate(const std::vector<std::vector<float>>& rewards,
                                         const std::pair<int,int> start_state, const std::pair<int,int> end_state,
                                         const std::pair<int,int> kern_shape) {
     // Define global params here.
@@ -83,40 +103,61 @@ std::vector<int> GetActionValueEstimate(const std::vector<std::vector<float>>& r
 
         // Weight for pseudo rewards.
         const float pseudo_reward_multiplier = 1.0;
+        // Env-specific rewards.
+        const std::vector<std::vector<float>>& rewards;
+        // Portion of the map that is seen by the agent.
         std::vector<std::vector<bool>> seen;
+        // All 4 possible visibility kernels.
         std::vector<std::vector<std::vector<bool>>> vis_kernels;
+        // Kernel Shape(H x W).
         std::pair<int,int> kern_shape;
+        // Environment Shape(H x W).
         std::pair<int,int> env_shape;
+        // Current state.
         std::pair<int,int> state;
+        // End state.
         const std::pair<int,int> end_state;
+        // Current direction.
+        Directions cur_direction;
 
-        // Define env functions.
-        Environment(std::pair<int,int> env_shape, std::pair<int,int> kern_shape,
-                    const std::pair<int,int> start_state,const std::pair<int,int> end_state) {
+        // Constructor to initialize env.
+        Environment(const std::vector<std::vector<float>>& rewards,
+                    const std::pair<int,int> start_state, const std::pair<int,int> end_state,
+                    const std::pair<int,int> kern_shape) {
+            rewards = rewards;
+            env_shape = std::make_pair(rewards.size(), rewards[0].size());
             seen = std::vector<std::vector<bool>>(env_shape.first,std::vector<bool>(env_shape.second,false));
             vis_kernels = {GetKernel(NORTH,kern_shape),GetKernel(SOUTH,kern_shape),
                            GetKernel(EAST,kern_shape),GetKernel(WEST,kern_shape)};
             kern_shape = kern_shape;
-            env_shape = env_shape;
             state = start_state;
             end_state = end_state;
+            cur_direction = NORTH;
         }
 
-        // Define env play() here.
+        // Play() for one-step.
         std::tuple<std::pair<int,int>,float,bool> play(int action) {
-            // TODO(SaiPraveenB) : Can you fill in the delta visibility calculations and visibility updating?
-            int delta_vis;
-            // Calculate delta(visibility)
-            // Update visibility
-
-            // Calculate psuedo rewards, actual reward
+            // Obtain action taken.
             const auto action_taken = actions[action];
+
+            // Update coordinates/state.
             int y_coord = (state.first+action_taken.first < 0 ? 0 : state.first+action_taken.first >= env_shape.first ? env_shape.first-1 : state.first+action_taken.first);
             int x_coord = (state.second+action_taken.second < 0 ? 0 : state.second+action_taken.second >= env_shape.second? env_shape.second-1 : state.first+action_taken.second);
             const auto new_state = std::make_pair(y_coord, x_coord);
+
+            // Update the seeing direction.
+            cur_direction = (action < 4 ? cur_direction : action % 4);
+
+            // Calculate change in visibility.
+            int delta_vis = GetDeltaAndUpdate(vis_kernels[(action < 4 ? cur_direction : action % 4)], seen, new_state);
+
+            // Produce a pseudo-reward signal for change in visibility.
             float pseudo_reward = delta_vis*pseudo_reward_multiplier;
+
+            // Compute total reward signal (0 actual reward if in the same state)
             float total_reward = (action < 4 ? (new_state == state ? 0 : rewards[y_coord][x_coord]) : 0) + pseudo_reward;
 
+            // Update state.
             state = new_state;
 
             // Return (new state, reward, is_completed)
@@ -137,6 +178,7 @@ std::vector<int> GetActionValueEstimate(const std::vector<std::vector<float>>& r
     std::random_device                  rand_dev;
     std::mt19937                        generator(rand_dev());
     std::uniform_int_distribution<int>  distr(0, q_values[0][0].size() - 1);
+    const Environment env(rewards,start_state,end_state,kern_shape);
 
     // Obtain trajectories
     std::vector<std::vector<std::tuple<std::pair<int,int>, int, float, std::pair<int,int>>>> trajectories;
@@ -145,7 +187,7 @@ std::vector<int> GetActionValueEstimate(const std::vector<std::vector<float>>& r
     for(int i=0;i<num_trajectories;i++) {
         // Start state (2-tuple) [int]
         std::vector < std::tuple < std::pair < int, int >, int, float, std::pair < int, int >> > cur_trajectory;
-        MinecraftPseudoEnvironment cur_env = env;
+        Environment cur_env(env);
         std::pair<int, int> cur_state(start_state);
         bool is_completed = false;
         int num_steps = 0;
@@ -192,6 +234,5 @@ std::vector<int> GetActionValueEstimate(const std::vector<std::vector<float>>& r
     }
     return q_values[start_state.first][start_state.second];
 }
-
 
 #endif //SUPER_DUPER_OCTO_LAMP_CPP_MC_H
