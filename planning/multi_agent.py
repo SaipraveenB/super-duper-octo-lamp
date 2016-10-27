11cbfd23b9561e3bd072b8446945602c6441a74f
JS Suhas <j.s.suhas@live.in>
2016-10-27 23:44:46 +0530

    Added upscaling of produced images; Added episodic image dump, GIF making.

--- planning/multi_agent.py
+++
@@ -1,9 +0,0 @@
-class MultiAgent:
-    def __init__(self, tf, grbm, vfs, env, num_sims):
-        self.tf = tf
-        self.rbm = grbm
-        self.env = [grbm for _ in range(num_sims)]
-        pass;
-
-    def run_once(self):
-        pass;
