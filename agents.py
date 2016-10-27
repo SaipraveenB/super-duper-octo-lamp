f3a2387f2428e931ae518b29fbfce14865562a7c
SaipraveenB <cs13b025@smail.iitm.ac.in>
2016-10-27 20:16:46 +0530

    Added code for agents.

--- agents.py
+++ agents.py
@@ -2,8 +2,7 @@ import numpy as np
 # Alpha agent.
 class Agent:
 
-
-    def __init__(self, tf, grbm, vfs, env ):
+    def __init__( self, tf, grbm, vfs, env ):
         self.tf = tf;
         self.grbm = grbm;
 
@@ -13,20 +12,25 @@ class Agent:
         self.env = env;
         pass;
 
-    def run_episode(self, max_steps=200):
+    def run_episode( self, max_steps=200 ):
         image, rewards = self.env.start();
-        # TODO: CHANGE THIS>
+        # TODO: CHANGE THIS
         image = np.zeros((28,28,3));
         # Main loop.
-        for k in range(0, 200):
+        for k in range(0, max_steps):
+
             image_in = np.array( [image.transpose([2,0,1])] );
-            vfunc = self.vfs.solve_one( image_in, 10 );
-            actionvals = np.array(vfunc[0][0][self.env.actions + self.env.cur_pos + (1, 1)]);
+            vfunc = self.vfs.solve_one( image_in, 10 )[0][0];
+            aindices = ( self.env.actions + self.env.cur_pos + (1, 1) ).transpose();
+            actionvals = np.array(vfunc[ list( aindices ) ]);
             action = np.argmax( actionvals );
 
             if np.random.rand() > 0.9:
                 action = np.random.randint(0,3);
             has_ended, mask, image, rewards = self.env.step( action );
+
+            print( "Now at:", self.env.cur_pos );
+
             if has_ended:
                 break;
 
